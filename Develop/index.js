5ce34cdfc0d528dee6ba4a86f95a3c98b8186550
Anna Chuapetcharasopon <143050387+ahgeak@users.noreply.github.com>
2023-12-02 12:22:11 -0800

    GitIgnore: added ds_store and gitignore; Index.js: added packages, added array of questions, started inquirer prompt; package.json: updated inquirer

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,75 @@
 // TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+const fs = require("fs");
+const generateMarkdown = require("./utils/generateMarkdown");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  {
+    type: "input",
+    name: "title",
+    message: "What is the title of your project?",
+  },
+  {
+    type: "input",
+    name: "description",
+    message: "Provide a short description explaining your project.",
+  },
+  {
+    type: "input",
+    name: "instalation",
+    message: "What are the steps required to install your project?",
+  },
+  {
+    type: "input",
+    name: "usage",
+    message: "Provide instructions for using your project.",
+  },
+  {
+    type: "input",
+    name: "credits",
+    message: "List your collaborators with links to their GitHub profiles.",
+  },
+  {
+    type: "checkbox",
+    name: "liscense",
+    message: "What liscense would you like to use for this project?",
+    choices: [
+      "Apache",
+      "Boost",
+      "BSD",
+      "Creative Commons",
+      "Eclipse",
+      "GNU",
+      "Hippocratic",
+      "IBM",
+      "ISC",
+      "MIT",
+      "Mozilla",
+      "Open Hardware",
+      "Open Software",
+      "Vim",
+      "Unlicense",
+      "UPL",
+      "Zlib",
+    ],
+  },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+  inquirer
+    .prompt(questions)
+    // .then((answers) =>
+    //     const readmePageContent = generateMarkdown(answers);
+    //     fs.writeToFile(); //I need to write this function still
+    // );
+
+    
+}
 
 // Function call to initialize app
 init();
